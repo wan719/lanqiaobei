44ea3bd954c14da28b4ee2b5fe8073e677cf5222
WAN719 <3425446714@qq.com>
2026-05-04 15:56:43 +0800

    修改目录结构，添加leetcode的题解

---
+++ lanqiaobei/java_100/BFS/java_032.java
@@ -0,0 +1,58 @@
+package java_100.BFS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class java_032 {
+    static int n;
+    static int m;
+    static int[][] g;
+    static int[][] dist;
+    static int[] dx={-1,1,0,0};
+    static int[] dy={0,0,-1,1};
+    static class Node{
+        int x,y;
+        Node(int x,int y){
+            this.x=x;
+            this.y=y;
+        }
+
+    }
+    static int bfs(){
+        Queue<Node> q=new LinkedList<>();
+        q.offer(new Node(0, 0));
+        dist[0][0]=0;
+        while (!q.isEmpty()) {
+           Node cur=q.poll();
+           for(int i=0;i<4;i++){
+               int nx=cur.x+dx[i];
+               int ny=cur.y+dy[i];
+               if(nx>=0&&nx<n&&ny>=0&&ny<m
+                       &&g[nx][ny]==0&&dist[nx][ny]==-1){
+                   dist[nx][ny]=dist[cur.x][cur.y]+1;
+                   q.offer(new Node(nx, ny));
+               }
+           }
+        }
+        return dist[n-1][m-1];
+    }
+    public static void main(String[] args)throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+        g=new int[n][m];
+        dist= new int[n][m];
+        for(int i=0;i<n;i++){
+            st=new StringTokenizer(br.readLine());
+            for(int j=0;j<m;j++){
+                g[i][j]=Integer.parseInt(st.nextToken());
+                dist[i][j]=-1;
+            }
+        }
+        System.out.println(bfs());
+    }
+}
